aca022e8072a77f1b587911f713eddba952b708d
SharonMutabonwa <mutaron37@gmail.com>
2022-12-30 19:56:46 +0200

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,9 +2,9 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('You cannot create an instance of an abstract class');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   get sqft() {
