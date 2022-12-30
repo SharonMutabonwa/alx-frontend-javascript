be085511ae3db867927f930e8e40d3e12dc6ea99
SharonMutabonwa <mutaron37@gmail.com>
2022-12-30 19:52:56 +0200

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 /* eslint no-underscore-dangle: 0 */
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('You cannot create an instance of an abstract class');
     }
+    this.sqft = sqft;
   }
 
   get sqft() {
