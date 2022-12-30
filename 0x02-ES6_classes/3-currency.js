aa8ad8d98cee13c09fa7cc2178e846c410b412b6
SharonMutabonwa <mutaron37@gmail.com>
2022-12-30 15:57:49 +0200

    currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw TypeError('code should be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw TypeError('name should be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return this._name(this._code);
+  }
+}
