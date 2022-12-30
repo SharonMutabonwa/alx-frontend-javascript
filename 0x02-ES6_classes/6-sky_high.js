fb74037fe144e60b26759f5194cb5a8f079234b4
SharonMutabonwa <mutaron37@gmail.com>
2022-12-30 20:47:04 +0200

    Inheritence

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -4,7 +4,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
