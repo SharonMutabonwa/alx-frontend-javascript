19e4379098b62d912e7d7cc3fd8784fcd0acf2b8
SharonMutabonwa <mutaron37@gmail.com>
2022-12-30 14:39:24 +0200

    getter and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,37 +2,38 @@
 
 export default class HolbertonCourse {
   constructor(name, length, student) {
-    this._name = name;
-    this._length = length;
-    this._student = student;
+    this.name = _name;
+    this.length = _length;
+    this.student = _student;
   }
-}
-get name() {
-  return this._name;
-}
 
-set name(name) {
-  if (typeof name !== 'string') throw TypeError('Name must be a string');
-  this._name = name;
-}
-get length() {
-  return this._length;
-}
+  get name() {
+    return this._name;
+  }
 
-set length(length) {
-  if (typeof length !== 'number') throw TypeError('Length must be a number');
-  this._length = length;
-}
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+  get length() {
+    return this._length;
+  }
 
-get students() {
-  return this._students;
-}
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
 
-set students(students) {
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
   if (
     !Array.isArray(students)
     || students.every((student) => typeof student !== 'string')
   ) throw TypeError('Students must be an array of strings');
 
-  this._students = students;
+    this._students = students;
+  }
 }
