c7022d622183777b961ec1624a5bcfe51b4e0f73
SharonMutabonwa <mutaron37@gmail.com>
2022-12-31 22:32:28 +0200

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -19,11 +19,13 @@ class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
+export default HolbertonClass;
+
+class StudentHolberton extends HolbertonClass {
+  constructor(firstName, lastName) {
+    super(HolbertonClass);
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -33,6 +35,10 @@ class StudentHolberton {
   get holbertonClass() {
     return this.holbertonClass;
   }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
@@ -41,5 +47,4 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents;
+export const listOfStudents = [student1, student2, student3, student4, student5];
